4ff3b4fef8e58bee1e9e6a2ac76e257bb077edab
Ricardo Tavares <ricardo.tavares@viewfromtheweb.com>
2021-12-01 13:23:57 +0000

    solve day 1

--- day_01/app.ts
+++ day_01/app.ts
@@ -16,3 +16,58 @@ In this example, there are 7 measurements that are larger than the previous meas
 Puzzle Input:
 https://adventofcode.com/2021/day/1/input
 */
+async function getPuzzleInput(url:string, refresh=false){
+    console.log(url + ' <==');
+    const input_filepath = './input.txt';
+    async function download(){
+        const session_cookie = '';//TODO automate this
+        try{
+            const res = await fetch(url, { headers:{ Cookie: session_cookie } });
+            const text = await res.text();
+            await Deno.writeTextFile(input_filepath, text);
+            return text;
+        }catch(error){ console.error(error); return false;}
+    }
+    try{ 
+        const saved_text = await Deno.readTextFile(input_filepath); 
+        if(!refresh){ return saved_text; }
+        return await download();
+    }catch(error){
+        console.log(error);
+        return await download();
+    }
+}
+function getDepthEntriesFromInput(input:string):number[]{
+    return input.split('\n').map(entry=>parseInt(entry));
+}
+function countIncreases(entries:number[]){
+    const count = entries.reduce((count, entry, index)=>{
+        if(index == 0){ return count; }
+        count = entry > entries[index - 1] ? count + 1 : count;
+        return count;
+    }, 0); 
+    return count;
+}
+function getRunningSums(entries:number[]){
+    let sums:number[] = [];
+    sums = entries.reduce((sums, entry, index)=>{
+        if(index + 2 >= entries.length){ return sums; }
+        sums.push(entry + entries[index + 1] + entries[index + 2]);
+        return sums;
+    }, sums);
+    return sums;
+}
+const test_entries = [ 199, 200, 208, 210, 200, 207, 240, 269, 260, 263 ];
+const puzzle_input = await getPuzzleInput('https://adventofcode.com/2021/day/1/input');
+const depth_entries = puzzle_input ? getDepthEntriesFromInput(puzzle_input) : [];
+console.log(depth_entries);
+console.log(
+    'submission 1: ', 
+    countIncreases(depth_entries), 
+    7 === countIncreases(test_entries)
+);
+console.log(
+    'submission 2: ', 
+    countIncreases(getRunningSums(depth_entries)), 
+    5 === countIncreases(getRunningSums(test_entries))
+);
