8b9421bdd3c02ce45f9810315c66c08cd0675928
Ricardo Tavares <ricardo.tavares@viewfromtheweb.com>
2021-12-01 10:55:54 +0000

    add day 1 challenge

---
+++ day_01/app.ts
@@ -0,0 +1,18 @@
+/* From https://adventofcode.com/2021/day/1 
+ Day 1: Sonar Sweep 
+To do this, count the number of times a depth measurement increases from the previous measurement. (There is no measurement before the first measurement.) In the example above, the changes are as follows:
+199 (N/A - no previous measurement)
+200 (increased) 1
+208 (increased) 2
+210 (increased) 3
+200 (decreased)
+207 (increased) 4
+240 (increased) 5
+269 (increased) 6
+260 (decreased) 
+263 (increased) 7
+In this example, there are 7 measurements that are larger than the previous measurement.
+
+Puzzle Input:
+https://adventofcode.com/2021/day/1/input
+*/
