d3b5c1053fc2907e5e5cb35f89e02dc723c81b79
Ricardo Tavares <ricardo.tavares@viewfromtheweb.com>
2021-12-08 10:51:03 +0000

    solve day 7

--- day_07/solution.ts
+++ day_07/solution.ts
@@ -57,19 +57,22 @@ const shiftFromPosition = (crabs:Crabs):Crabs =>{
     crabs.from_position ++;
     return crabs;
 }
-const totalFuel = (crabs:Crabs):number =>{
-    let fuel = crabs.left.reduce((fuel, count, index)=>fuel+(count*index), 0);
-    fuel = crabs.right.reduce((fuel, count, index)=>fuel+(count*index), fuel);
+const totalFuel = (crabs:Crabs, constant_rate=true):number =>{
+    const calculateRate = (fuel:number, count:number, index:number) =>{
+        if(constant_rate){ return fuel + (count * index); }
+        const all_indexes = Array.from(Array(index + 1).keys()).reduce((t, i)=>t+i, 0)
+        return fuel + (count * all_indexes);
+    };
+    let fuel = crabs.left.reduce(calculateRate, 0);
+    fuel = crabs.right.reduce(calculateRate, fuel);
     return fuel;
 }
-const findLeastFuel = (crabs:Crabs) =>{
+const findLeastFuel = (crabs:Crabs, constant_rate=true) =>{
     const least = { fuel:0, position: 0 }
-    least.fuel = totalFuel(crabs);
-    log(0, least.fuel);
+    least.fuel = totalFuel(crabs, constant_rate);
     for(let position = 1; position <= crabs.max_position; position ++){
         crabs = shiftFromPosition(crabs);
-        const current_fuel = totalFuel(crabs);
-        log(position, current_fuel);
+        const current_fuel = totalFuel(crabs, constant_rate);
         if(current_fuel < least.fuel){ 
             least.fuel = current_fuel; 
             least.position = position; 
@@ -82,31 +85,31 @@ const testPart1 = async (input:string):Promise<boolean> =>{
     const puzzle_input = await puzzle.parseInput(input);
     const crabs = parseCrabs(puzzle_input.blocks[0][0]);
     const least = findLeastFuel(crabs);
-    return least.position === 2;
+    return least.fuel === 37;
 }
 const solvePart1 = async ():Promise<number> =>{
     const puzzle_input = await puzzle.parseInput();
     const crabs = parseCrabs(puzzle_input.blocks[0][0]);
-    log(crabs);
-    return 1;
     const least = findLeastFuel(crabs);
-    return least.position;
+    return least.fuel;
 }
 const testPart2 = async (input:string):Promise<boolean> =>{
     const puzzle_input = await puzzle.parseInput(input);
-
-    return false;
+    const crabs = parseCrabs(puzzle_input.blocks[0][0]);
+    const least = findLeastFuel(crabs, false);
+    return least.fuel === 168;
 }
 const solvePart2 = async ():Promise<number> =>{
     const puzzle_input = await puzzle.parseInput();
-
-    return 2;
+    const crabs = parseCrabs(puzzle_input.blocks[0][0]);
+    const least = findLeastFuel(crabs, false);
+    return least.fuel;
 }
 const test_input = `16,1,2,0,4,2,7,1,2,14`;
 
 const part1_correct = await testPart1(test_input);
 const part1 = await solvePart1();
-log(',.-~=/* part 1: ', part1, part1_correct);
+log('    part 1: ', part1, part1_correct);
 const part2_correct = await testPart2(test_input);
 const part2 = await solvePart2();
-log(',.-~=/* part 2: ', part2, part2_correct);
+log('    part 2: ', part2, part2_correct);
