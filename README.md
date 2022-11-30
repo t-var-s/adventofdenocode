3e935f05c3f8f96b733cf30db79abe377c11431c
Ricardo Tavares <ricardo.tavares@viewfromtheweb.com>
2022-11-30 20:17:29 +0000

    apply deno fmt

--- README.md
+++ README.md
@@ -2,14 +2,17 @@
 
 ![adventofdenocode_demo](https://user-images.githubusercontent.com/6660327/202920591-d4f18f64-203e-4540-afb9-658d6660e664.gif)
 
-
 ```
 deno run --allow-net --allow-read --allow-write script.ts [YEAR OF ADVENT OF CODE] [YOUR SESSION COOKIE HERE]
 ```
 
-
-In 2021, to warm-up and start learning Typescript and Deno, I made a script to download the input and the first challenge for each day. Run the command above with the year and your session cookie from adventofcode.com, this will create a subfolder on the current directory for each available day. Besides the input.txt, an initial solution.ts file will be saved with the text from that day's challenge as a comment and some boilerplate code that I've found to be useful as all puzzles involve handling some line-by-line input.
-
+In 2021, to warm-up and start learning Typescript and Deno, I made a script to
+download the input and the first challenge for each day. Run the command above
+with the year and your session cookie from adventofcode.com, this will create a
+subfolder on the current directory for each available day. Besides the
+input.txt, an initial solution.ts file will be saved with the text from that
+day's challenge as a comment and some boilerplate code that I've found to be
+useful as all puzzles involve handling some line-by-line input.
 
 https://adventofcode.com
 
--- tools.ts
+++ tools.ts
@@ -1,34 +1,34 @@
 import { format } from "https://deno.land/std@0.117.0/datetime/mod.ts";
-export const log = (...anything:any):string =>{
-    const timestamp = format(new Date(), "HH:mm:ss");
-    if(anything.length > 2){
-        console.log(timestamp, anything[0], anything[1], anything[2]);
-    }else if(anything.length > 1){
-        console.log(timestamp, anything[0], anything[1]);
-    }else{
-        console.log(timestamp, anything[0]);
-    }
-    return timestamp;
+export const log = (...anything: any): string => {
+  const timestamp = format(new Date(), "HH:mm:ss");
+  if (anything.length > 2) {
+    console.log(timestamp, anything[0], anything[1], anything[2]);
+  } else if (anything.length > 1) {
+    console.log(timestamp, anything[0], anything[1]);
+  } else {
+    console.log(timestamp, anything[0]);
+  }
+  return timestamp;
+};
+export const logList = (list: any[]): string => {
+  const timestamp = format(new Date(), "HH:mm:ss");
+  console.log(timestamp);
+  list.forEach((item, index) => console.log(`[${index}]`, item));
+  return timestamp;
+};
+export const intval = (text_number: string): number => {
+  return parseInt(text_number);
+};
+export async function getPuzzleInput(): Promise<string | boolean> {
+  const input_filepath = "./input.txt";
+  try {
+    const saved_text = await Deno.readTextFile(input_filepath);
+    return saved_text;
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
 }
-export const logList = (list:any[]):string =>{
-    const timestamp = format(new Date(), "HH:mm:ss");
-    console.log(timestamp);
-    list.forEach((item, index)=>console.log(`[${index}]`, item));
-    return timestamp;
-}
-export const intval = (text_number:string):number =>{
-    return parseInt(text_number);
-}
-export async function getPuzzleInput():Promise<string|boolean>{
-    const input_filepath = './input.txt';
-    try{ 
-        const saved_text = await Deno.readTextFile(input_filepath); 
-        return saved_text;
-    }catch(error){
-        console.log(error);
-        return false;
-    }
-}
-export function getEntriesFromInput(input:string):string[]{
-    return input.split('\n');
+export function getEntriesFromInput(input: string): string[] {
+  return input.split("\n");
 }
